271aef0484709f854366e56543031fe68b9aa7c3
Felipe Henao <pipehenaogomez@gmail.com>
2023-09-04 22:54:13 -0500

    added results for all instances

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PSP import RCPSP
 start=time.time()
 opt=0
 gaps=[]
+nonOpt=[]
+nonOptStatus=[]
 #objs=[]
 for m in range(1,49):
     for n in range(1,11):
@@ -15,7 +17,9 @@ for m in range(1,49):
         if problem.status == 2:
             opt+=1
         else:
-            gaps=gaps+[problem.gap]
+            gaps.append(problem.gap*100)
+            nonOpt.append(inst+sample)
+            nonOptStatus.append(problem.status)
 
 with open("results.txt", "w") as res:
     res.write("Number of optimal results = "+str(opt))
@@ -26,4 +30,10 @@ with open("results.txt", "w") as res:
     res.write('\n')
     res.write("Gaps: ")
     res.write(str(gaps))
+    # res.write('\n')
+    # res.write("non-optimal instances: ")
+    # res.write(str(nonOpt))
+    # res.write('\n')
+    # res.write("Non-optimal status: ")
+    # res.write(str(nonOptStatus))
         
