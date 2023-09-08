7c492b996c0d994323226bcd74e33cff371066e6
Felipe Henao <pipehenaogomez@gmail.com>
2023-09-07 19:38:21 -0500

    Change to print table of results for each instance

--- mainRCPSP.py
+++ mainRCPSP.py
@@ -7,37 +7,24 @@ gaps=[]
 nonOpt=[]
 nonOptStatus=[]
 objs=[]
-for m in range(1,2):
-    for n in range(1,11):
-        inst=str(m)
-        sample=str(n)
-
-        problem=RCPSP(inst,sample)
-        problem.solve()
-        if problem.status == 2:
-            opt+=1
-            objs.append(problem.objVal)
-        else:
-            gaps.append(problem.gap*100)
-            nonOpt.append(inst+sample)
-            nonOptStatus.append(problem.status)
-
 with open("results.txt", "w") as res:
-    res.write("Number of optimal results = "+str(opt))
+    res.write(f"{'Instance' : ^10}{'Sample' : ^10}{'Objective' : ^15}{'Gap' : ^10}{'Runtime' : ^10}")
     res.write('\n')
-    res.write("Number of non-optimal results = "+str(480-opt))
+    res.write(''.join(['-' for _ in range(55)]))
     res.write('\n')
-    res.write("Running time = "+ str(time.time()-start))
-    res.write('\n')
-    res.write("Gaps: ")
-    res.write(str(gaps))
-    res.write('\n')
-    res.write("Objectives: ")
-    res.write(str(objs))
-    # res.write('\n')
-    # res.write("non-optimal instances: ")
-    # res.write(str(nonOpt))
-    # res.write('\n')
-    # res.write("Non-optimal status: ")
-    # res.write(str(nonOptStatus))
-        
+    for m in range(1,49):
+        for n in range(1,11):
+            inst=str(m)
+            sample=str(n)
+
+            problem=RCPSP(inst,sample)
+            problem.solve()
+            if problem.status == 2:
+                opt+=1
+                objs.append(problem.objVal)
+            else:
+                gaps.append(problem.gap*100)
+                nonOpt.append(inst+sample)
+                nonOptStatus.append(problem.status)
+            res.write(f"{inst : ^10}{sample : ^10}{str(round(problem.objVal,1)) : ^15}{str(round(problem.gap,2)) : ^10}{str(round(problem.runtime,2)) : ^10}")
+            res.write('\n')
