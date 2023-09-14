eea8bdae5eb2b51402010aff3477f988c29d8fb7
Felipe Henao <pipehenaogomez@gmail.com>
2023-09-14 11:55:31 -0500

    Corrections

--- mainRCMPSP_Resource.py
+++ mainRCMPSP_Resource.py
@@ -7,7 +7,7 @@ gaps=[]
 nonOpt=[]
 nonOptStatus=[]
 objs=[]
-NoProy=[1,2,3]
+NoProj=[1,2,3]
 alphaVal=[0.0,0.5,1.0]
 with open("resultsResource.txt", "w") as res:
     res.write(f"{'Instance' : ^10}{'No. Proj' : ^10}{'Res Cost Type' : ^15}{'Alpha' : ^10}{'Objective' : ^15}{'Gap' : ^10}{'Runtime' : ^10}{'Resources' : ^40}")
@@ -16,17 +16,17 @@ with open("resultsResource.txt", "w") as res:
     res.write('\n')
     for m in range(1,49):
         inst=str(m)
-        for P in range(2,4):
+        for p in range(len(NoProj)):
             for type in range(1,3):
                 for a in range(len(alphaVal)):
                     alpha=alphaVal[a] 
                     
-                    problem=RCMPSP_Resource(inst,P,alpha,1,type)
+                    problem=RCMPSP_Resource(inst,NoProj(p),alpha,1,type)
                     problem.solve()
 
                     if type==1:
-                        res.write(f"{inst : ^10}{P : ^10}{'Constant' : ^15}{str(alpha) : ^10}{str(round(problem.objVal,1)) : ^15}{str(round(problem.gap,2)) : ^10}{str(round(problem.runtime,2)) : ^10}{str(problem.resources) : ^40}")
+                        res.write(f"{inst : ^10}{NoProj(p) : ^10}{'Constant' : ^15}{str(alpha) : ^10}{str(round(problem.objVal,1)) : ^15}{str(round(problem.gap,2)) : ^10}{str(round(problem.runtime,2)) : ^10}{str(problem.resources) : ^40}")
                         res.write('\n') 
                     else:
-                        res.write(f"{inst : ^10}{P : ^10}{'Variable' : ^15}{str(alpha) : ^10}{str(round(problem.objVal,1)) : ^15}{str(round(problem.gap,2)) : ^10}{str(round(problem.runtime,2)) : ^10}{str(problem.resources) : ^40}")
+                        res.write(f"{inst : ^10}{NoProj(p) : ^10}{'Variable' : ^15}{str(alpha) : ^10}{str(round(problem.objVal,1)) : ^15}{str(round(problem.gap,2)) : ^10}{str(round(problem.runtime,2)) : ^10}{str(problem.resources) : ^40}")
                         res.write('\n')
