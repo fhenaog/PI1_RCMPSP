e24a247aba861e0bee9631570b8c98ce6ad593bb
Felipe Henao <pipehenaogomez@gmail.com>
2023-10-25 18:15:21 -0500

    Added main for mix

---
+++ mainRCMPSP_Mix.py
@@ -0,0 +1,32 @@
+import time
+from PSP import RCMPSP_Mix
+
+start=time.time()
+opt=0
+gaps=[]
+nonOpt=[]
+nonOptStatus=[]
+objs=[]
+NoProj=[1,2,3]
+alphaVal=[0.0,0.5,1.0]
+with open("resultsMix.txt", "w") as res:
+    res.write(f"{'Instance' : ^10}{'No. Proj' : ^10}{'Res Cost Type' : ^15}{'Alpha' : ^10}{'Objective' : ^15}{'Gap' : ^10}{'Runtime' : ^10}{'Resources' : ^40}{'Projects' : ^30}")
+    res.write('\n')
+    res.write(''.join(['-' for _ in range(150)]))
+    res.write('\n')
+    for m in range(1,49):
+        inst=str(m)
+        for p in range(len(NoProj)):
+            for type in range(1,3):
+                for a in range(len(alphaVal)):
+                    alpha=alphaVal[a] 
+
+                    problem=RCMPSP_Mix(inst,NoProj[p],alpha,type)
+                    problem.solve()
+
+                    if type==1:
+                        res.write(f"{inst : ^10}{NoProj[p] : ^10}{'Constant' : ^15}{str(alpha) : ^10}{str(round(problem.objVal,1)) : ^15}{str(round(problem.gap,2)) : ^10}{str(round(problem.runtime,2)) : ^10}{str(problem.resources) : ^40}{str(problem.projects) : ^30}")
+                        res.write('\n') 
+                    else:
+                        res.write(f"{inst : ^10}{NoProj[p] : ^10}{'Variable' : ^15}{str(alpha) : ^10}{str(round(problem.objVal,1)) : ^15}{str(round(problem.gap,2)) : ^10}{str(round(problem.runtime,2)) : ^10}{str(problem.resources) : ^40}{str(problem.projects) : ^30}")
+                        res.write('\n')
