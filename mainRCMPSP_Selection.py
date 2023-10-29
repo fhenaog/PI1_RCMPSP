6e44b50c5f9cfb36c6e631b1b91ba8f81f4c6682
Felipe Henao <pipehenaogomez@gmail.com>
2023-10-29 14:32:25 -0500

    Added write for project selection

--- mainRCMPSP_Selection.py
+++ mainRCMPSP_Selection.py
@@ -7,21 +7,25 @@ gaps=[]
 nonOpt=[]
 nonOptStatus=[]
 objs=[]
-NoProj=[2,3,5]
-alphaVal=[0.0,0.5,1.0]
+NoProj=[2,3,5,10] #Number of projects
+alphaVal=[0.0,0.25,0.5,0.75,1.0] #alpha values
+instances=[[13],[48],[29],[24],[13,48,29,24]] #instances (last one is for mixing)
+result="write" # "solve" --- "write" to write the problem, "solve" to solve the problem
+n=1
 with open("resultsSelection.txt", "w") as res:
     res.write(f"{'Instance' : ^10}{'No. Proj' : ^10}{'Alpha' : ^10}{'Objective' : ^15}{'Gap' : ^10}{'Runtime' : ^10}{'Projects' : ^30}")
     res.write('\n')
     res.write(''.join(['-' for _ in range(95)]))
     res.write('\n')
-    for m in range(1,2):
-        inst=str(m)
-        for p in range(len(NoProj)-1):
-                for a in range(len(alphaVal)):
-
-                    alpha=alphaVal[a] 
+    for inst in instances:
+        for p in NoProj:
+                for alpha in alphaVal:
                     
-                    problem=RCMPSP_Selection(inst,NoProj[p],alpha)
-                    problem.solve()
-                    res.write(f"{inst : ^10}{NoProj[p] : ^10}{str(alpha) : ^10}{str(round(problem.objVal,1)) : ^15}{str(round(problem.gap,2)) : ^10}{str(round(problem.runtime,2)) : ^10}{str(problem.projects) : ^30}")
-                    res.write('\n') 
+                    problem=RCMPSP_Selection(inst,p,alpha)
+                    if result=="solve":
+                        problem.solve()
+                        res.write(f"{inst : ^10}{NoProj[p] : ^10}{str(alpha) : ^10}{str(round(problem.objVal,1)) : ^15}{str(round(problem.gap,2)) : ^10}{str(round(problem.runtime,2)) : ^10}{str(problem.projects) : ^30}")
+                        res.write('\n') 
+                    elif result=="write":
+                        problem.write(n)
+                        n+=1
